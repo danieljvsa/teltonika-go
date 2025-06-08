ba5843fd9bfcd067360dfdc566126d4091ffbda4
danieljvsa <danielviana18@gmail.com>
2025-06-08 19:59:44 +0100

    feat: Add return for encoder functions

--- pkg/encoders.go
+++ pkg/encoders.go
@@ -1,9 +1,19 @@
 package teltonika_go
 
-func encodeCodec8(data []byte, dataLength int64)    {}
-func encodeCodec8Ext(data []byte, dataLength int64) {}
-func encodeCodec12(data []byte, dataLength int64)   {}
-func encodeCodec13(data []byte, dataLength int64)   {}
-func encodeCodec14(data []byte, dataLength int64)   {}
-func encodeCodec15(data []byte, dataLength int64)   {}
-func encodeCodec16(data []byte, dataLength int64)   {}
+import decoder_domain "github.com/danieljvsa/teltonika-go/internal/decoder"
+
+func encodeCodec8(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+
+func encodeCodec8Ext(data []byte, dataLength int64) (*decoder_domain.CodecData, error) {
+	return nil, nil
+}
+
+func encodeCodec12(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+
+func encodeCodec13(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+
+func encodeCodec14(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+
+func encodeCodec15(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+
+func encodeCodec16(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
