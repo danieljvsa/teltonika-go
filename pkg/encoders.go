6c7f0ae0c6c64e4efa1a22377e40778017a8db5c
danieljvsa <danielviana18@gmail.com>
2025-06-08 20:17:51 +0100

    chore: export functions for enconding

--- pkg/encoders.go
+++ pkg/encoders.go
@@ -2,18 +2,18 @@ package teltonika_go
 
 import decoder_domain "github.com/danieljvsa/teltonika-go/internal/decoder"
 
-func encodeCodec8(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+func EncodeCodec8(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
 
-func encodeCodec8Ext(data []byte, dataLength int64) (*decoder_domain.CodecData, error) {
+func EncodeCodec8Ext(data []byte, dataLength int64) (*decoder_domain.CodecData, error) {
 	return nil, nil
 }
 
-func encodeCodec12(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+func EncodeCodec12(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
 
-func encodeCodec13(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+func EncodeCodec13(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
 
-func encodeCodec14(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+func EncodeCodec14(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
 
-func encodeCodec15(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+func EncodeCodec15(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
 
-func encodeCodec16(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
+func EncodeCodec16(data []byte, dataLength int64) (*decoder_domain.CodecData, error) { return nil, nil }
